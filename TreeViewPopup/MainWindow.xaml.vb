f45b71a41971f050ae4baf33d0e9c7bca64003b2
Christoph Grün <c.gruen@imagiro.net>
2018-06-19 15:18:55 +0200

    First Version

--- TreeViewPopup/MainWindow.xaml.vb
+++ TreeViewPopup/MainWindow.xaml.vb
@@ -1,3 +1,11 @@
 ﻿Class MainWindow
+    Private Sub Do_SayHello(sender As Object, e As RoutedEventArgs)
+        Dim SourceButton = TryCast(sender, Button)
+        If SourceButton IsNot Nothing Then
+            MsgBox("Hello " & SourceButton.Content.ToString)
+        Else
+            MsgBox("Hello")
 
+        End If
+    End Sub
 End Class
